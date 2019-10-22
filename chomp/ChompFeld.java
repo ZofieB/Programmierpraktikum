915cb54ae4433981e8e508965c44ec4939eb5110
ZofieB <sophie.m.blum@gmail.com>
2019-10-22 11:05:28 +0200

    methods initializeSpielfeld and print Spielfeld updated

--- chomp/ChompFeld.java
+++ chomp/ChompFeld.java
@@ -7,15 +7,20 @@ public class ChompFeld extends Spielfeld{
     private int [][] feld; 
 
     public void initializeSpielfeld() {
-        java.util.Arrays.fill(feld, 0);
+        for ( int i = 0; i < this.vertical; i++) {
+            for ( int j = 0; j < this.horizontal; j++) {
+            feld[i][j] = 0;
+            }
+        }
     }
 
     @Override
     public void printSpielfeld() {
         for ( int i = 0; i < this.vertical; i++) {
-            for ( int j = 0; j < this.horizontal; j++)
+            for ( int j = 0; j < this.horizontal; j++) {
             System.out.print(feld[i][j] + "\t");
             System.out.print("\n");
+            }
         }
     }
 }
