39aab1a708f9b26857a43f0bf7469e94adb2d85b
ZofieB <55540680+ZofieB@users.noreply.github.com>
2019-10-22 10:25:35 +0200

    Delete Protokollierbar.java

--- Protokollierbar.java
+++
@@ -1,6 +0,0 @@
-public interface Protokollierbar{
-    //Stack-speicher  zum Protokollieren der Züge
-
-    public void addSpielzug(Spielzug spielzug);
-    public void removeSpielzug(Spielzug spielzug);
-}
