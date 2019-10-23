700b5b7acde99698c7b4431af16563a03a150ee2
ZofieB <sophie.m.blum@gmail.com>
2019-10-23 14:25:24 +0200

    Spielfeld für Chomp funktioniert jetzt (Initialisierung und Ausgabe)

--- chomp/ChompFeld.java
+++ chomp/ChompFeld.java
@@ -7,7 +7,8 @@ public class ChompFeld extends Spielfeld{
     private int [][] feld; 
 
     public void initializeSpielfeld() {
-        this.feld = new feld[this.vertical][this.horizontal]
+        this.feld = new int[this.vertical][this.horizontal];
+
         for ( int i = 0; i < this.vertical; i++) {
             for ( int j = 0; j < this.horizontal; j++) {
                 this.feld[i][j] = 0;
@@ -19,9 +20,9 @@ public class ChompFeld extends Spielfeld{
     public void printSpielfeld() {
         for ( int i = 0; i < this.vertical; i++) {
             for ( int j = 0; j < this.horizontal; j++) {
-            System.out.print(feld[i][j] + "\t");
-            System.out.print("\n");
+                System.out.print(feld[i][j] + "\t");
             }
+            System.out.println();
         }
     }
 }
