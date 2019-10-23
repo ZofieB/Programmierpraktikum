17550f27aafbbe43f57108ce241df487c2601a7d
ZofieB <sophie.m.blum@gmail.com>
2019-10-23 15:32:21 +0200

    Konstruktor ChompSpielzug

--- chomp/ChompSpielzug.java
+++ chomp/ChompSpielzug.java
@@ -5,6 +5,11 @@ import absclasses.*;
 public class ChompSpielzug extends Spielzug{
     protected int ykoordinate;
 
+    ChompSpielzug(int x, int y) {
+        this.xkoordinate = x;
+        this.ykoordinate = y;
+    }
+
     public int getYkoordinate() {
         return this.ykoordinate;
     }
