bbc19fa03bcb37aa00edebc7f2cbe712cb881367
ZofieB <sophie.m.blum@gmail.com>
2019-10-23 14:51:48 +0200

    removeSpielzug braucht keine Parameter

--- absclasses/Protokollierbar.java
+++ absclasses/Protokollierbar.java
@@ -5,6 +5,6 @@ public interface Protokollierbar {
     public Stack<Spielzug> szstack =  new Stack<Spielzug>();
 
     public void addSpielzug(Spielzug spielzug);
-    public void removeSpielzug(Spielzug spielzug);
+    public void removeSpielzug();
     public Stack getSzstack();
 }
