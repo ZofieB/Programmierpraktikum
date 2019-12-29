d79ef41a1d66825f6773d54d463ad44e9aad6bb3
ZofieB <sophie.m.blum@gmail.com>
2019-12-29 14:57:25 +0100

    Erste Funktionalität der GUI. Sie wird jetzt gestartet aber es ist noch nicht das gelbe vom EI!

--- serverclient/Client.java
+++ serverclient/Client.java
@@ -1,69 +1,48 @@
 package serverclient;
 
+import java.awt.*;
 import java.io.*;
 import java.net.*;
+import java.nio.channels.OverlappingFileLockException;
 import java.util.*;
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class Client{
+
+public class Client extends Application {
+
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Soll eine Verbindung mit dem Server aufgebaut werden?");
-        System.out.println("1 - Ja \t 0 - Nein");
-        int trylogin = scan.nextInt();
-        scan.nextLine();
-        if(trylogin == 1) {
-            try{
-                Socket server = new Socket("localhost", 6666);
-                BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
-                
-                //Login Versuch (Methode login disconnected client wenn login fehlgeschlagen)
-                System.out.println("Benutzername:");
-                String client_benutzername = scan.nextLine();
-                out.write(client_benutzername);
-                out.newLine();
-                out.flush();
-                System.out.println("Passwort:");
-                out.write(scan.nextLine());
-                out.newLine();
-                out.flush();
-                boolean login = true;
-                if(in.readLine().equals("111")){
-                    System.out.println(in.readLine());
-                }
-                
-                //Nachrichten empfangen
-                MessageListener messages = new MessageListener(in);
-                messages.start();
+        Application.launch(args);
+    }
 
-                //Abmeldung vom Server
-                System.out.println("Zum Logout 101 eingeben!");
-                while(login) {
-                    String input = scan.nextLine();
-                    if(input.equals("101")){
-                        send_server_message(input, "101", server);
-                        login = false;
-                    }
-                    else{
-                        send_server_message(input, "100", server);
-                    }
-                }
-                scan.close();
-                server.close();
-            }catch(UnknownHostException e) {
-                System.out.println("Cannot find host.");
-            }catch (IOException e) {
-                System.out.println("Error connecting to host.");
-            }
-        }
-        else if(trylogin == 0) {
-            System.out.println("Verbindung wird nicht hergestellt!");
-        }
-        else{
-            System.out.println("Eingabe war nicht gültig. Verbindung wird abgebrochen");
-        }
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        // Create the FXMLLoader
+        FXMLLoader loader = new FXMLLoader();
+        // Path to the FXML File
+        String fxmlDocPath = "/home/sophie/Documents/Programmierpraktikum/serverclient/ClientWindow.fxml";
+        FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
 
+        // Create the Pane and all Details
+        AnchorPane root = (AnchorPane) loader.load(fxmlStream);
+
+        // Create the Scene
+        Scene scene = new Scene(root);
+        // Set the Scene to the Stage
+        stage.setScene(scene);
+        // Set the Title to the Stage
+        stage.setTitle("Connected to Server");
+        // Display the Stage
+        stage.show();
     }
 
     static public void send_server_message(String message, String code, Socket server)throws IOException{
@@ -75,3 +54,23 @@ public class Client{
         out.flush();
     }
 }
+
+/*
+        //Login Versuch (Methode login disconnected client wenn login fehlgeschlagen)
+        System.out.println("Benutzername:");
+        String client_benutzername = scan.nextLine();
+        out.write(client_benutzername);
+        out.newLine();
+        out.flush();
+        System.out.println("Passwort:");
+        out.write(scan.nextLine());
+        out.newLine();
+        out.flush();
+        boolean login = true;
+        if(in.readLine().equals("111")){
+        System.out.println(in.readLine());
+        }
+
+        //Nachrichten empfangen
+
+*/
